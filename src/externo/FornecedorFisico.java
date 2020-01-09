3d3376e3fdacc860e20f5adb70d02473dd48f5a9
Julio Cesar <rasjuliocesar@gmail.com>
2020-01-09 16:18:17 -0300

    Realizado Tópico 4 do Arquivo PDF

---
+++ src/externo/FornecedorFisico.java
@@ -0,0 +1,63 @@
+package externo;
+
+public class FornecedorFisico extends Fornecedor{
+
+	private int cpf;
+	private double horasTrabalhadas;
+	private double valorPorHora;
+	private String servicosDisponiveis;
+	
+	//Construtor Padrão - Herança Classe FornecedorxPessoa
+	public FornecedorFisico(String nome, String endereco, String telefone) {
+		super(nome, endereco, telefone);
+	}
+	
+	//Construtor Opcional 1
+	public FornecedorFisico(String nome, String endereco, String telefone, int cpf) {
+		super(nome, endereco, telefone);
+		this.cpf = cpf;
+	}
+	
+	//Get and Set
+	public int getCpf() {
+		return this.cpf;
+	}
+	
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	
+	public double getHorasTrabalhadas() {
+		return this.horasTrabalhadas;
+	}
+	
+	public void setHorasTrabalhadas(double horasTrabalhadas) {
+		this.horasTrabalhadas = horasTrabalhadas;
+	}
+	
+	public double getValorPorHora() {
+		return this.valorPorHora;
+	}
+	
+	public void setValorPorHora(double valorPorHora) {
+		this.valorPorHora = valorPorHora;
+	}
+	
+	public String getServicosDisponiveis() {
+		return this.servicosDisponiveis;
+	}
+	
+	public void setServicosDisponiveis(String servicosDisponiveis) {
+		this.servicosDisponiveis = servicosDisponiveis;
+	}
+	
+	//Método para Calcular o Valor Completo do Serviço (Multiplicar Quantidade de Horas Trabalhada x Valor da Hora).
+	public double honorariosPorServico(){
+		return this.getHorasTrabalhadas() * this.getValorPorHora();
+	}
+	
+	@Override
+	public String toString() {
+		return "Nome: " + getNome() + " Endereco: " + getEndereco() + " Telefone: " + getTelefone() + " CPF: " + this.getCpf() + " Valor Crédito: " + getValorCredito() + " Valor Dívida: " + getValorDivida(); 
+	}
+}
